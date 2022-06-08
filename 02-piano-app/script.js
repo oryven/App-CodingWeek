e00e3709b1b5d2af84a85f35fff85a3cdabf6793
Orazio <orazioventrella@hotmail.it>
2022-06-08 17:16:41 +0200

    feat: created app piano

---
+++ 02-piano-app/script.js
@@ -0,0 +1,33 @@
+console.log('ciao');
+
+const keyElements = document.querySelectorAll('.key');
+
+const notes = {
+    do: '01-do.mp3',
+    dodiesis: '02-dodiesis.mp3',
+    re: '03-re.mp3',
+    rediesis: '04-rediesis.mp3',
+    mi: '05-mi.mp3',
+    fa: '06-fa.mp3',
+    fadiesis: '07-fadiesis.mp3',
+    sol: '08-sol.mp3',
+    soldiesis: '09-soldiesis.mp3',
+    la: '10-la.mp3',
+    ladiesis: '11-ladiesis.mp3',
+    si: '12-si.mp3'
+}
+
+function playSoud(key) {
+    console.log(keyElements);
+    const audioElement = new Audio();
+    const note = notes[key]
+    audioElement.src = 'sounds/' + note;
+    audioElement.play();
+}
+
+keyElements.forEach(function (keyElement) {
+    keyElement.addEventListener('touchend', function () {
+        const key = keyElement.id;
+        playSoud(key)
+    } );
+})
