9c5c4815edf398468dcf5ae8333d97e0f2460f49
Orazio <orazioventrella@hotmail.it>
2022-06-09 18:39:34 +0200

    feat: add new SpeechSynthesisUtterance

--- 03-talkinDuck/script.js
+++ 03-talkinDuck/script.js
@@ -1,7 +1,24 @@
-console.log('ciao');
-
-const text = document.querySelector('textarea');
+const textArea = document.querySelector('textarea');
 const playButton = document.querySelector('button');
 const pitchBar = document.querySelector('input');
 const duckFigure = document.querySelector('figure');
 
+playButton.addEventListener('click', function() {
+    const textLength = textArea.value.trim().length;
+    console.log(textLength);
+    if (textLength > 0) {
+        talk();
+    }
+});
+
+function talk() {
+    const text = textArea.value;
+    const pitch = pitchBar.value;
+
+    const utterance = new SpeechSynthesisUtterance(text);
+
+    utterance.volume = 1;
+    utterance.rate = 1;
+    utterance.pitch = pitch;
+    speechSynthesis.speak(utterance);
+}
