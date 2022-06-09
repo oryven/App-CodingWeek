bcabb6f8f93ef4747b1bbc4eb75471afd16fc523
Orazio <orazioventrella@hotmail.it>
2022-06-09 17:43:50 +0200

    feat: layout complete

---
+++ 03-talkinDuck/script.js
@@ -0,0 +1,7 @@
+console.log('ciao');
+
+const text = document.querySelector('textarea');
+const playButton = document.querySelector('button');
+const pitchBar = document.querySelector('input');
+const duckFigure = document.querySelector('figure');
+
