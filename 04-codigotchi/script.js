65b7164a4c14e6f14fe70618fa144cfbd04022c7
Orazio <orazioventrella@hotmail.it>
2022-06-13 22:02:36 +0200

    Feat: UI codigotchi

---
+++ 04-codigotchi/script.js
@@ -0,0 +1,8 @@
+console.log('ciao');
+
+const mic = document.getElementById('mic');
+const screen = document.getElementById('screen');
+const panelData = document.getElementById('panel-data');
+
+const commands = ['mangia', 'balla', 'dormi'];
+
