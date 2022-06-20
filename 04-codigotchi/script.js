fc45bc87aac0f150ad315945c929447b5ebcbd84
Orazio <orazioventrella@hotmail.it>
2022-06-20 23:03:50 +0200

    FEAT: Add method SpeechRecognition

--- 04-codigotchi/script.js
+++ 04-codigotchi/script.js
@@ -1,4 +1,3 @@
-console.log('ciao');
 
 const mic = document.getElementById('mic');
 const screen = document.getElementById('screen');
@@ -6,3 +5,36 @@ const panelData = document.getElementById('panel-data');
 
 const commands = ['mangia', 'balla', 'dormi'];
 
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; 
+
+const reco = new SpeechRecognition();
+
+function onStart() {
+    console.log('ciao');
+    panelData.classList.add('listening');
+    reco.start();
+};
+
+function onResult(event) {
+    const testo = event.results[0][0].transcript;
+    console.log(testo);
+
+    const action = commands.find(function(command) {
+        return testo.toLowerCase().includes(command);
+    });
+
+    console.log('action', action);
+
+    const actionClassname = 'codigotchi-screen_' + action;
+
+    screen.classList.add(actionClassname);
+
+    panelData.classList.remove('listening');
+
+    setTimeout(function() {
+        screen.classList.remove(actionClassname);
+    }, 2000);
+}
+
+mic.addEventListener('click', onStart);
+reco.addEventListener('result', onResult);
